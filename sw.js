886e0ef1f35671ae03a731ca9c523b03fcd177ff
Brian Ingles <github@emeraldwalk.com>
2026-03-30 21:24:31 -0500

    Added proper manifest

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE = 'passport-cam-v1'
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png']
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {})))
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()))
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
+})
